99fd257d0753d0c7256e0d0bc3238b7b32ff60af
Zibing Zhang <zibing.zha@gmail.com>
2021-12-07 01:13:40 -0500

    day 7 ez

--- 2021/day5.py
+++ 2021/day5.py
@@ -42,16 +42,69 @@ To avoid the most dangerous areas, you need to determine the number of points wh
 
 Consider only horizontal and vertical lines. At how many points do at least two lines overlap?
 
+--- Part Two ---
+
+Unfortunately, considering only horizontal and vertical lines doesn't give you the full picture; you need to also consider diagonal lines.
+
+Because of the limits of the hydrothermal vent mapping system, the lines in your list will only ever be horizontal, vertical, or a diagonal line at exactly 45 degrees. In other words:
+
+    An entry like 1,1 -> 3,3 covers points 1,1, 2,2, and 3,3.
+    An entry like 9,7 -> 7,9 covers points 9,7, 8,8, and 7,9.
+
+Considering all lines from the above example would now produce the following diagram:
+
+1.1....11.
+.111...2..
+..2.1.111.
+...1.2.2..
+.112313211
+...1.2....
+..1...1...
+.1.....1..
+1.......1.
+222111....
+
+You still need to determine the number of points where at least two lines overlap. In the above example, this is still anywhere in the diagram with a 2 or larger - now a total of 12 points.
+
+Consider all of the lines. At how many points do at least two lines overlap?
 """
+from collections import Counter
 
 
 def mapper(text):
-    return text.split("\n")
+    inp = text.split("\n")
+    inp = [line.split(' -> ') for line in inp]
+    inp = [(list(map(int, line[0].split(','))), list(map(int, line[1].split(',')))) for line in inp]
+    return inp
 
 
-def part1(text):
-    pass
+def part1(inp):
+    counter = Counter()
+    for line in inp:
+        (x0, y0), (x1, y1) = line
+        if x0 == x1:
+            for i in range(min(y0, y1), max(y0, y1) + 1):
+                counter[(x0, i)] += 1
+        elif y0 == y1:
+            for i in range(min(x0, x1), max(x0, x1) + 1):
+                counter[(i, y0)] += 1
+    return len(list(filter(lambda k: counter[k] >= 2, counter)))
 
 
-def part2(text):
-    pass
+def part2(inp):
+    counter = Counter()
+    for line in inp:
+        (x0, y0), (x1, y1) = line
+        if x0 > x1:
+            x0, y0, x1, y1 = x1, y1, x0, y0
+        if x0 == x1:
+            for i in range(min(y0, y1), max(y0, y1) + 1):
+                counter[(x0, i)] += 1
+        elif y0 == y1:
+            for i in range(x0, x1 + 1):
+                counter[(i, y0)] += 1
+        else:
+            sign = 1 if y1 > y0 else -1
+            for dx, y in enumerate(range(y0, y1 + sign, sign)):
+                counter[(x0 + dx, y)] += 1
+    return len(list(filter(lambda k: counter[k] >= 2, counter)))
